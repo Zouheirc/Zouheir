39c6564260b04f2bd5e03bcded11c369a4cc1f77
Zouheirc <zouheir@live.com>
2021-05-29 12:25:49 -0500

    drums login skills div bugs update

--- app.js
+++ app.js
@@ -1,169 +1,36 @@
 //jshint esversion:6
 
-/*	gallery */
-// $(document).ready(function(){
-//
-//   $(".filter-button").click(function(){
-//     var value = $(this).attr('data-filter');
-//
-//     if(value == "all")
-//     {
-//       $('.filter').show('1000');
-//     }
-//     else
-//     {
-//       $(".filter").not('.'+value).hide('3000');
-//       $('.filter').filter('.'+value).show('3000');
-//
-//     }
-//
-//     if ($(".filter-button").removeClass("active")) {
-//       $(this).removeClass("active");
-//     }
-//     $(this).addClass("active");
-//   });
-// });
-
-/*	end gallery */
-// $(document).ready(function(){
-//   $(".fancybox").fancybox({
-//     openEffect: "none",
-//     closeEffect: "none"
-//   });
-// });
-
 // Certifications
 
 var gallery = document.querySelector('#gallery');
 var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
 var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
 var resizeAll = function () {
-    var altura = getVal(gallery, 'grid-auto-rows');
-    var gap = getVal(gallery, 'grid-row-gap');
-    gallery.querySelectorAll('.gallery-item').forEach(function (item) {
-        var el = item;
-        el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
-    });
+  var altura = getVal(gallery, 'grid-auto-rows');
+  var gap = getVal(gallery, 'grid-row-gap');
+  gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+    var el = item;
+    el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+  });
 };
 gallery.querySelectorAll('img').forEach(function (item) {
-    item.classList.add('byebye');
-    if (item.complete) {
-        console.log(item.src);
-    }
-    else {
-        item.addEventListener('load', function () {
-            var altura = getVal(gallery, 'grid-auto-rows');
-            var gap = getVal(gallery, 'grid-row-gap');
-            var gitem = item.parentElement.parentElement;
-            gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
-            item.classList.remove('byebye');
-        });
-    }
+  item.classList.add('byebye');
+  if (item.complete) {
+    console.log(item.src);
+  }
+  else {
+    item.addEventListener('load', function () {
+      var altura = getVal(gallery, 'grid-auto-rows');
+      var gap = getVal(gallery, 'grid-row-gap');
+      var gitem = item.parentElement.parentElement;
+      gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+      item.classList.remove('byebye');
+    });
+  }
 });
 window.addEventListener('resize', resizeAll);
 gallery.querySelectorAll('.gallery-item').forEach(function (item) {
-    item.addEventListener('click', function () {
-        item.classList.toggle('full');
-    });
-});
-
-
-// Drums App
-
-
-let numberOfDrumButtons = document.querySelectorAll(".drum").length;
-
-for (let i = 0; i < numberOfDrumButtons; i++) {
-
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    let buttonInnerHTML = this.innerHTML;
-    makeSound(buttonInnerHTML);
-    buttonAnimation(buttonInnerHTML);
+  item.addEventListener('click', function () {
+    item.classList.toggle('full');
   });
-}
-
-document.addEventListener("keypress", function(event) {
-  makeSound(event.key);
-  buttonAnimation(event.key);
-});
-
-function makeSound(key) {
-  switch (key) {
-    case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-
-    case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-
-    case "s":
-      var tom3 = new Audio('sounds/tom-3.mp3');
-      tom3.play();
-      break;
-
-    case "d":
-      var tom4 = new Audio('sounds/tom-4.mp3');
-      tom4.play();
-      break;
-
-    case "j":
-      var snare = new Audio('sounds/snare.mp3');
-      snare.play();
-      break;
-
-    case "k":
-      var crash = new Audio('sounds/crash.mp3');
-      crash.play();
-      break;
-
-    case "l":
-      var kick = new Audio('sounds/kick-bass.mp3');
-      kick.play();
-      break;
-
-    default:
-      console.log(key);
-  }
-}
-
-function buttonAnimation(currentKey) {
-  let activeButton = document.querySelector("." + currentKey);
-  activeButton.classList.add("pressed");
-  setTimeout(function() {
-    activeButton.classList.remove("pressed");
-  }, 100);
-}
-
-
-
-
-// Login app
-
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => {
-  container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-  container.classList.remove("right-panel-active");
-});
-
-$(document).ready(function(){
-    $('.progress-value > span').each(function(){
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        },{
-            duration: 1500,
-            easing: 'swing',
-            step: function (now){
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
 });

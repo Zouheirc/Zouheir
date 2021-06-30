024ae81c3791863086b71ddf92d8221810bac89e
Zouheirc <zouheir@live.com>
2021-06-29 22:25:35 -0500

    fixing bugs

--- app.js
+++ app.js
@@ -4,20 +4,6 @@
 // font-size: 0.9rem;
 // font-family: inherit;
 
-// display navbar collapse on click
-
-
-// var hamburger = document.getElementById('hamburger');
-// var menu = document.getElementById('navbar--middle');
-// menu.style.display = "none";
-// hamburger.addEventListener('click', function() {
-//   this.classList.toggle("change");
-//   if (menu.style.display === "none") {
-//     menu.style.display = "block";
-//   } else {
-//     menu.style.display = "none";
-//   }
-// });
 
 
 
